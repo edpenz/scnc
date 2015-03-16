b28c7de5fa4a35fdca777310bb4b6bd69d6a899b
Edward Peek <edwardpeek@gmail.com>
2015-03-16 21:28:49 +1300

    Import window sized to show more updates.

--- Client/src/nz/ac/squash/windows/ImportWindow.java
+++ Client/src/nz/ac/squash/windows/ImportWindow.java
@@ -98,7 +98,7 @@ public class ImportWindow extends JDialog {
 
         GridBagLayout gridBagLayout = new GridBagLayout();
         gridBagLayout.columnWidths = new int[] { 0, 0, 0 };
-        gridBagLayout.rowHeights = new int[] { 0, 0, 50, 0, 0, 0 };
+        gridBagLayout.rowHeights = new int[] { 0, 0, 50, 500, 0, 0 };
         gridBagLayout.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
         gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0,
                 Double.MIN_VALUE };
