a7239237fc6e03d0b8401903aa7cb6393191c4e9
Edward Peek <edwardpeek@gmail.com>
2015-05-20 22:47:59 +1200

    Added daily attendance report.

--- Client/make_reports.bat
+++ Client/make_reports.bat
@@ -1,3 +1,5 @@
 java.exe -classpath "..\Client\bin;..\MemberLib\bin;..\MemberLib\lib\*;..\Client\lib\*" nz.ac.squash.reports.MakeLadder
 
 java.exe -classpath "..\Client\bin;..\MemberLib\bin;..\MemberLib\lib\*;..\Client\lib\*" nz.ac.squash.reports.ReportNumberOfTurnups
+
+java.exe -classpath "..\Client\bin;..\MemberLib\bin;..\MemberLib\lib\*;..\Client\lib\*" nz.ac.squash.reports.ReportAttendance
